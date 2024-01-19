d637ddbf8f96e10bd1390efa2b8759d7f5b5ad58
Angelo Gabriel <angelogabriel.contact@gmail.com>
2024-01-19 16:52:50 -0300

    First class: Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class App {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Starting project");
+		
+		Position position = new Position(2, 4);
+		
+		System.out.println(position);
 	}
 
 }
